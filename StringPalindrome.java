caf7fa03cf00b43cd316a6f6e2683154be91df1f
Ajith Kumar <45239303+AjithKumarChennai@users.noreply.github.com>
2019-03-19 10:23:43 +0530

    Update StringPalindrome.java

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,22 +1,36 @@
 //Checking whether the String is palidrome or not
-public class FirstClass {
+public class MainClass {
 
-	public static void main(String[] args) {
-		
-		String str = "malayalam";
-		
-		String rev = "";
-		
-		for(int i=str.length()-1 ; i>=0; i--) {
-			
-			rev = rev + str.charAt(i);
-			
-		}
-		
-		String res = (str.equals(rev)) ? "Palindrome" : "Not a palidrome";
-		
-		System.out.println(res);
-		
-	}
+    public static void main(String args[]) {
+
+        String str = "malayalam";
+
+        char[] ch = str.toCharArray();
+
+        int length = ch.length;
+
+        int div = length/2;
+
+        int count = 0;
+
+        for (int i = 0, j = length-1; i < length-1 && j > 0; i++, j--) {
+
+            if (ch[i] == ch[j] && i != div) {
+
+                count++;
+
+            } else {
+
+                break;
+
+            }
+
+        }
+
+        String result = count == div ? "Panlindrome" : "Not a palindrome";
+
+        System.out.println(result);
+
+    }
 
 }
